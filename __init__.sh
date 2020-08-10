be14ef340b84b25a9de0295026c49319503af7ff
William R Barker <wbarker@cas.org>
2020-08-10 14:22:13 +0000

    Added error function with backtracing

--- __init__.sh
+++ __init__.sh
@@ -34,3 +34,54 @@ function debug::off {
 
     return
 }
+
+#=============================================================================
+# ERROR
+#=============================================================================
+function error {
+    debug::off
+
+    local -i indent=0
+    local -i lineno_count=0
+    local -i bash_source_count=1
+    local -i trace_count="$(expr ${#BASH_SOURCE[@]} - 1)"
+
+    local -i lineno="${BASH_LINENO[${lineno_count}]}"
+    local bash_source="${BASH_SOURCE[${bash_source_count}]}"
+    local func_name="${FUNCNAME[${bash_source_count}]}"
+
+    tput sgr0 >&2
+    tput bold >&2
+    tput setaf 1; printf "%b" "\U1F480"
+    tput setaf 7; printf ' ['  >&2
+    tput setaf 5; printf "%s" "$(basename "${bash_source}"):" >&2
+    tput setaf 4; printf "%s" "${func_name}():"  >&2
+    tput setaf 3; printf "%s" "${lineno}"  >&2
+    tput setaf 7; printf "%s " '] -'  >&2
+    tput setaf 1; printf "%s\n" " ERROR: $@" >&2
+
+    while [[ "${bash_source_count}" -ne "${trace_count}" ]]; do
+        (( lineno_count += 1 ))
+        (( bash_source_count += 1 ))
+        (( indent += 4 ))
+
+        lineno="${BASH_LINENO[${lineno_count}]}"
+        bash_source="${BASH_SOURCE[${bash_source_count}]}"
+        func_name="${FUNCNAME[${bash_source_count}]}"
+
+        tput setaf 3; printf "%-${indent}s%b " "" "\U27A1" >&2
+        tput setaf 7; printf '['  >&2
+        tput setaf 5; printf "%s" "$(basename "${bash_source}"):" >&2
+        tput setaf 4; printf "%s" "${func_name}():"  >&2
+        tput setaf 3; printf "%s" "${lineno}"  >&2
+        tput setaf 7; printf "%s " '] -'  >&2
+        tput setaf 6; printf "%s\n" \
+            "$(sed -n "${lineno}p" "${bash_source}" | sed -r 's|^\s+||')" >&2
+    done
+
+    tput sgr0 >&2
+
+    exit 1
+
+    return 1
+}
